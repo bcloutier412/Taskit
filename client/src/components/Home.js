22a90e9ee384fa773706d6cc66340ef2f002c30c
Dino <brandoncloutier412@gmail.com>
2023-04-03 22:33:58 -0700

    refactor

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -116,7 +116,6 @@ const NewTask = ({ setShowAddTask, currentUser, todos, setTodos }) => {
     return (
         <div className="wrapper absolute h-full w-full flex justify-center bg-slate-500/50">
             <div className="relative w-full max-w-lg bg-white sm:rounded-lg rounded-none border h-min p-5 text-center mt-12">
-                <div>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="16"
@@ -128,7 +127,6 @@ const NewTask = ({ setShowAddTask, currentUser, todos, setTodos }) => {
                     >
                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
                     </svg>
-                </div>
                 <h1 className="font-semibold">New Task</h1>
                 <form
                     className="flex flex-col"
